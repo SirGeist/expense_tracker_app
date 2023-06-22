1704506de58b88d0580782254fa600fb7510c75b
Peter Huynh <trinh1234533@yahoo.com>
2023-06-22 15:07:37 -0500

    showing a date picker
    working with Futures by creating a text date and displaying it next to calendar

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -18,6 +19,30 @@ class _NewExpenseState extends State<NewExpense> {
   // Second way
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+
+    // First date is today - 1 year, but everything is today
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+
+    // Code afterwards will wait for the previous info
+    // Now we are assigning the user picked date for display
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+
+    
+  }
 
   @override
   void dispose() {
@@ -43,17 +68,51 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: '\$ ',
-              label: Text('Amount'),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+
+                    // Displays date text next to the calendar icon
+                    // Also we use the ! at the end of _selectDate to force date that selectDate will
+                    // never be null (because we checked for it previously)
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Row(children: [
+            // Cancel Button
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                // Pops the modal by pressing the "cancel button"
+                Navigator.pop(context);
+              },
               child: const Text('Cancel'),
             ),
             ElevatedButton(
