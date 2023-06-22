d37ffa288fda656b0e201f70e1dcfd8ee6852557
Peter Huynh <trinh1234533@yahoo.com>
2023-06-22 10:30:48 -0500

    added padding and displaying more expense information

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:expense_tracker_app/expense.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
@@ -14,7 +15,7 @@ class ExpensesList extends StatelessWidget {
     // Build/create items if they are visible
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
     );
   }
 }
